7292da5da9dfa4536a53622b3359001427a39b61
pasindusampath <pasindub32@gmail.com>
2023-08-06 18:35:18 +0530

    test_commit

--- src/main/java/lk/ijse/gdse63/aad/app/AppInitializer.java
+++ src/main/java/lk/ijse/gdse63/aad/app/AppInitializer.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse63.aad.app;
 
+import lk.ijse.gdse63.aad.app.config.ApplicationConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitializer{
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        //XML Application Context - Configure with xml
+        ctx.register(ApplicationConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+    }
 }
+//ioc container
+//spring container
+//bean factory
--- src/main/java/lk/ijse/gdse63/aad/app/config/ApplicationConfig.java
+++ src/main/java/lk/ijse/gdse63/aad/app/config/ApplicationConfig.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse63.aad.app.config;
 
+import lk.ijse.gdse63.aad.app.sample.MyBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration//Source for bean definition
+@ComponentScan("lk.ijse.gdse63.aad.app.sample")
 public class ApplicationConfig {
+
+    @Bean(name = "first")
+    public MyBean getMyBean(){
+        return new MyBean();
+    }
+
 }
